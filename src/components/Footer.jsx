60c3504ec99dad3c0350c0823f9444a345f07b6d
Anshuman Parmar <anshumansp16@gmail.com>
2023-10-22 15:50:36 +0530

    Added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { Box, TextField } from '@mui/material';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import AddIcon from '@mui/icons-material/Add';
 
 const Footer = () => {
-  return (
-    <div>
-      This is the Footer Section
-    </div>
-  )
-}
+  const inputStyle = {
+    width: '60%',
+    backgroundColor: '#FFF',
+    position: 'fixed',
+    bottom: '10px',
+    left: '20%',
+    right: '20%',
+    borderRadius: '8px',
+    padding: '8px',
+  };
 
-export default Footer
+  const adornmentStyle = {
+    marginRight: '10px',
+    display: "flex",
+    cursor: "pointer"
+  };
+
+  return (
+    <Box>
+      <TextField
+        id="outlined-basic"
+        placeholder="Type your message here..."
+        variant="outlined"
+        style={inputStyle}
+        InputProps={{
+          startAdornment: (
+            <div style={adornmentStyle}>
+              <ExpandLessIcon color="action" sx={{marginRight: "10px"}} />
+              <AddIcon color="action" />
+            </div>
+          ),
+          endAdornment: (
+            <div style={adornmentStyle}>
+              <TelegramIcon color="action" />
+            </div>
+          ),
+          style: {
+            backgroundColor: '#FFF',
+            outline: "disabled"
+          },
+        }}
+      />
+    </Box>
+  );
+};
+
+export default Footer;
