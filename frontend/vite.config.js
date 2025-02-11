c61f3bb7611612ffa49e327fb477992cdc8e1386
Anshuman Parmar <anshumansp16@gmail.com>
2025-02-11 19:53:31 +0530

    Fixed Frontend Errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,11 +16,11 @@ export default defineConfig({
     outDir: "dist",
     assetsDir: "assets",
     sourcemap: false,
-    manifest: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined,
-        format: "es",
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
       },
     },
   },
