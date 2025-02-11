28152338f667d169e1a9c9a67e9fcf490e142acf
Anshuman Parmar <anshumansp16@gmail.com>
2025-02-11 17:56:34 +0530

    Added Documentation and Updated Code

--- frontend/src/firebase.config.js
+++ frontend/src/firebase.config.js
@@ -3,15 +3,17 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyALIJeAFtwoKqs8wOWgpFEuBR0EDAhCFqc",
-  authDomain: "project-netaji.firebaseapp.com",
-  projectId: "project-netaji",
-  storageBucket: "project-netaji.appspot.com",
-  messagingSenderId: "272067118468",
-  appId: "1:272067118468:web:9a0e17c2778efbb97349d1",
-  measurementId: "G-5EP3WMZ741",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
+export default firebaseConfig;
